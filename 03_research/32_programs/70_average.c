10c2c077fa563e9dc9705147fa413de7d8ae13c2
Masatsugu Kitadai lab wsl <b8123026@gmail.com>
2021-09-26 16:06:52 +0900

    git

--- 03_research/32_programs/70_average.c
+++ 03_research/32_programs/70_average.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-PROGRAM NAME :fft.c
+PROGRAM NAME :average.c
 AUTHER  : Masatsugu Kitadai
 DATE    :
 ******************************************************************************/
@@ -7,11 +7,11 @@ DATE    :
 #include <stdlib.h>
 #include <math.h>
 #define ch 2
-FILE *fp1;
+#include "files/fp.h"
 /*********************************   MAIN   *********************************/
 int read(char filename1[])
 {
-    // ファイルの読み込み
+    // 補正ファイルの読み込み
 
     fp1 = fopen(filename1, "r");
     if (filename1 == NULL)
@@ -42,6 +42,34 @@ int read(char filename1[])
     fclose(fp1);
 
     data_long = i;
+
+    // 起動・停止時刻の読み込み
+    
+    fp1 = fopen(filename1, "r");
+    if (filename1 == NULL)
+    {
+        printf("Error! I can't open the file.\n");
+        exit(0);
+    }
+    // データの格納
+
+    int i = 0;
+    int N = 1000;
+    int num, data_long;
+    double ch1, ch2;
+    double value[N][ch];
+    int number[N];
+
+    // ch1:drag, ch2:lift
+
+    while ((fscanf(fp1, "%d\t%lf\t%lf", &num, &ch1, &ch2)) != EOF)
+    {
+        number[i] = num;
+        value[i][1] = ch1;
+        value[i][2] = ch2;
+        // printf("%d\t%lf\t%lf\n", number[i], value[i][1], value[i][2]);
+        i = i + 1;
+    }
 }
 
 // int main ()
